4cec30a6dd9d28e673dee70bf26ec4565c38c980
M0haty <cmc.mohaty@gmail.com>
2023-04-02 01:10:51 +0100

    Adding third activity

--- act3/recieve.sh
+++ act3/recieve.sh
@@ -2,23 +2,24 @@
 
 key="private"
 
-# Wait for message from sender
+# Wait for message
+
 echo "Waiting for message from sender..."
 while [ ! -f message.txt ]
 do
   sleep 1
 done
 
-# Read message and HMAC from file
+# Read info
+
 message=$(cat message.txt | cut -d '|' -f 1)
 hmac=$(cat message.txt | cut -d '|' -f 2)
 
-# Verify HMAC
+# Verify authentecity
+
 if [ "$hmac" == "$(echo -n "$message" | openssl dgst -sha256 -hmac "$key" | awk '{print $2}')" ]; then
   echo "Message received successfully! Message: $message"
 else
   echo "Message verification failed. HMAC mismatch!"
 fi
-
-# Clean up
 rm message.txt
